c23969b0c5ab6684d9a267e751b22cc72e3f48f8
Hosam Saleh <73144863+HosamSaleh1@users.noreply.github.com>
2020-12-19 19:22:20 +0200

    Create main.py

---
+++ TranspositionEncrypt/main.py
@@ -0,0 +1,42 @@
+import pyperclip
+
+def main():
+    myMessage = "Common sense is not so common."
+    myKey = 8
+
+ciphertext = encryptMessage(myKey, myMessage)
+
+"""
+Print the encryption string in ciphertext to the screen,
+with a | (called "pipe" character) after it in case there are space at
+the end of the encrypted message.
+"""
+print(ciphertext + "|")
+
+# Copy the encrypted string in ciphertext to the clipboard.
+pyperclip.copy(ciphertext)
+
+def encryptMessage(key, message):
+    # Each string in ciphertext represents a column in the grid.
+    ciphertext = [""] * key
+    
+    # Loop through each column in ciphertext.
+    for col in rang(key):
+        pointer = col
+        
+        # Keep looping until pointer goes past the length of the message.
+        while pointer < len(message):
+            # Place the character at pointer in message at the end of the
+            # current column in the ciphertext list.
+            ciphertext[col] += message[pointer]
+            
+            # Move pointer over.
+            pointer += key
+    # Convert the ciphertext list into a single string value and return it.
+    return "".join(ciphertext)
+"""
+If transpositionEncrypt.py is run (instead of imported as a module)
+call the main() function.
+"""
+if __name__ = "__main__":
+    main()
